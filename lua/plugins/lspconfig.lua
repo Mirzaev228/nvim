c3efd798aa98290d0175617bfe1202075fd8dadf
mirzaev <arsen@mirzaev.sexy>
2023-02-14 08:12:39 +1000

    добавлен готовый набросок для HTML

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,10 +1,19 @@
+-- Активация вещания готовых набросков 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 -- Инициализация "ms-jpq/coq_nvim"
 local coq = require('coq')
 
 -- Инициализация LSP-серверов
 
--- Настройка и инициализация "psalm" (LSP-сервер для PHP)
+-- Инициализация "psalm" (LSP-сервер для PHP)
 require('lspconfig').psalm.setup {
   on_attach = lspconfig_on_attach,
   coq.lsp_ensure_capabilities()
+}
+
+-- Инициализация "vscode-html-language-server" (готовый набросок для HTML)
+require('lspconfig').html.setup {
+  capabilities = capabilities
 }
