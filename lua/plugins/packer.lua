96fdd735ad43312a774053757152868c46b03c53
mirzaev <arsen@mirzaev.sexy>
2024-01-05 23:33:43 +0700

    fidhet update

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -166,7 +166,7 @@ return require('packer').startup(function(use)
 		run = 'composer install'
 	}
 
-	-- Аналог LSP-сервера для поддержки оболочки fish
+	-- LSP-сервер для Fish Shell
 	use 'dag/vim-fish'
 
 	-- Цветовая тема
@@ -181,7 +181,6 @@ return require('packer').startup(function(use)
 	-- Форматировщик Prettier
 	use {
 		'prettier/vim-prettier',
-		run = 'yarn install --frozen-lockfile --production',
-		config = function() require('plugins/vim-prettier') end
+		run = 'yarn install --frozen-lockfile --production'
 	}
 end)
